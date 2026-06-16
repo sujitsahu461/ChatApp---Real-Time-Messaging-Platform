e301c2a1d34906938fb59ef73918920435943217
Sujit Sahu <smartsujit7334@gmail.com>
2026-06-16 13:40:13 +0530

    Serve media files in production mode using django.views.static.serve

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic.base import RedirectView
 from webapp.views import *
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('favicon.ico', RedirectView.as_view(url='/static/webapp/favicon.svg', permanent=True), name='favicon'),
@@ -22,7 +22,7 @@ urlpatterns = [
     path('get_messages/<int:user_id>/', get_messages),
     path('get_group_messages/<int:group_id>/', get_group_messages),
     path('get_unread_counts/', get_unread_counts),
+    
+    # Serve media files in production & development (needed for free-tier Render deployments)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
